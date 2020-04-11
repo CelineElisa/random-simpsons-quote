343c39f291395208d7091e2eba0fbc55f31414f7
Céline Cottier <cottier.celine@gmail.com>
2020-04-11 21:26:54 +0200

    application done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import QuoteCard from './components/QuoteCard'
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const sampleSimpson ={
+  quote: "I live in a single room above a bowling alley...and below another bowling alley.",
+  character: "Frank Grimes",
+  image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FFrankGrimes.png?1497567511887',
+}
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={ simpson:sampleSimpson};
+    this.getSimpson = this.getSimpson.bind(this);
+  }
+  getSimpson() {
+    // Send the request
+    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        this.setState({
+          simpson: data[0],
+        });
+    });
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <QuoteCard simpson={this.state.simpson}/>
+     <button type="button" onClick={this.getSimpson}>Get Simpsons quote</button>
     </div>
   );
+  }
 }
 
 export default App;
